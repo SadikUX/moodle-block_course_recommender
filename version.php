472d4c0b5eab155559b35ae3aa31f7d2a6d407d7
Sadik Mert <sadikmert@hotmail.de>
2026-08-11 19:42:18 +0200

    Version updated.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'block_course_recommender';
-$plugin->release      = '1.2.2';
-$plugin->version      = 2025091401;
+$plugin->release      = '1.3.0';
+$plugin->version      = 2026081100;
 $plugin->requires     = 2022041900;
 $plugin->maturity     = MATURITY_STABLE;
